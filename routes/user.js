4329df4abd7a3b18c31c103db53ff3b538c7017b
RajSM139 <rajesh.medimojo@gmail.com>
2022-08-08 22:16:53 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,9 @@
 var User = require('../controllers/user');
 
 module.exports = app => {
-
+	app.get("/health-check", (req, res) => {
+	  res.send({ success: true, message: "Test Success" });
+	});
 	app.get('/user/check-user/:adhaar', User.checkUser);
 	app.post('/user/create-user', User.validate('createUser'), User.createUser);
 
